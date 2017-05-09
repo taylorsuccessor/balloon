d1bf051d5629be2836a36a5512e43917fbd71ee4
taylorsuccessor <taylorsuccessor@gmail.com>
2017-05-09 11:41:21 +0300

    database change

--- config.php
+++ config.php
@@ -32,7 +32,7 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'balloony_maw');
+define('DB_DATABASE', 'balloon');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
