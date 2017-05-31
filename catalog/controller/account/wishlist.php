09b74082a0122dd1cb3779d4482ba8a0f74af48b
mohammad_gholeh <mohammad_gholeh@hotmail.com>
2017-05-31 11:06:36 +0300

    - check wishlist

--- catalog/controller/account/wishlist.php
+++ catalog/controller/account/wishlist.php
@@ -176,6 +176,25 @@ class ControllerAccountWishList extends Controller {
 //			}
 
 			if ($this->customer->isLogged()) {
+
+				if(isset($this->request->get['ajaxRequest'])) {
+				if(isset($this->request->get['customer_session']) && isset($this->request->post['product_id'])) {
+					$session = $this->request->get['customer_session'];
+					$product_id = $this->request->post['product_id'];
+				}elseif(!isset($this->request->get['customer_session']) && !isset($this->request->post['product_id']))
+				{
+					$session = "no session saved in this file second try";
+					$product_id = "product id null";
+				}
+				$filename = 'reviews.txt';
+				$handle = fopen($filename,"w");
+				    fwrite($handle,$session);
+					fwrite($handle,$product_id);
+				echo "success Add";
+				fclose($handle);
+			}
+
+
 				// Edit customers cart
 				$this->load->model('account/wishlist');
 
